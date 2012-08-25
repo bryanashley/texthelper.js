035d5f9e9a96baa569b5034ae1693e29c1a3a1d6
Mike Bonds <mikeb@fluidmedia.com>
2012-08-25 10:55:13 -0400

    Wrote the truncate method - still needs to be tested. Added a utility functions for triming strings

--- texthelper.js
+++ texthelper.js
@@ -1,16 +1,40 @@
 (function(window, undefined) {
 	// Useful whitespace regEx available throughout texthelperjs.
-	var regSpace     = /\s+/g,
-	    regTrimLeft  = /^\s+/,
-	    regTrimRight = /\s+$/,
-	    regTrimBoth  = /(^\s+|\s+$)/g,
-	    regTrimLarge = /[\t\n\r]/g;
+	var regwsGlobal = /\s+/g,
+	    regwsLeft   = /^\s+/,
+	    regwsRight  = /\s+$/,
+	    regwsBoth   = /(^\s+|\s+$)/g,
+	    regwsLarge  = /[\t\n\r]/g;
 
 	var texthelper = {
 		version: '0',
 
-		truncate: function(text, len, omission) {
+		truncate: function(text, length, omission) {
+			var i         = 0,
+			    truncated = '',
+			    sentence  = this.trimString(text);,
+			    omission  = omission || '...';
 
+			if(typeof length === 'number') {
+				length = parseInt(length);
+			} else {
+				return false;
+			}
+
+			if(sentence.length > length) {
+				for( ; i <= length; i++) {
+					if(i === sentence.length) {
+						break;	
+					}
+
+					if(i != length) {
+						truncated += sentence.charAt(i);
+					} else {
+						truncated += omission;
+					}
+				}
+				return truncated;
+			}
 		},
 
 		excerpt: function(text, interest, radius) {
@@ -31,7 +55,12 @@
 
 		html: function(text) {
 
-		}
+		},
+
+		// Helpful utility methods.
+		trimBoth: function(text) {
+			retrun text.toString().replace(regwsBoth, '');
+		},
 	};
 
 	// Make texthelper available throughout the global namespace.
