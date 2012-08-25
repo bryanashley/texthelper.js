7357ba9f558d83246ec06a0c8228fda2cbb9627b
Mike Bonds <mikeb@fluidmedia.com>
2012-08-24 20:03:54 -0400

    Added texthelper.js

---
+++ texthelper.js
@@ -0,0 +1,27 @@
+(function(window, undefined) {
+	// Useful whitespace regEx available throughout texthelperjs.
+	var regSpace     = /\s+/g,
+		regTrimLeft  = /^\s+/,
+		regTrimRight = /\s+$/,
+		regTrimBoth  = /(^\s+|\s+$)/g,
+		regTrimLarge = /[\t\n\r]/g;
+
+	var texthelper = {
+		version: '0',
+
+		truncate: function(text, len, omission) {
+
+		},
+
+		excerpt: function(text, interest, radius) {
+
+		},
+
+		highLight: function(text, keyWord) {
+
+		}
+	};
+
+	// Make texthelper available throughout the global namespace.
+	window.texthelper = texthelper;
+})(window);
